71f65cb1a03106683ea8342a3477939ba37b0e21
Satoshi628 <180442042@ccmailg.meijo-u.ac.jp>
2023-02-20 21:59:30 +0900

    cache_resource

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ from deep_AI import deep_ai_func
 # None
 
 
-# @st.cache_data
+@st.cache_resource
 def image_load(image):
     img = Image.open(image)
     return img
 
 
-# @st.cache_data
+@st.cache_resource
 def Deep_AI_load(**kwargs):
     img = deep_ai_func(**kwargs)
     return img
